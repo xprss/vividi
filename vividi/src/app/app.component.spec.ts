b5ba1742751493369078118c9668a2ec296d648f
xprss <vincenzo.sagristano@yahoo.com>
2024-12-07 16:33:29 +0100

    slaaaay fixed it was using the wrong version of angular

--- vividi/src/app/app.component.spec.ts
+++ vividi/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'vividi-frontend' title`, () => {
+  it(`should have the 'vividi' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vividi-frontend');
+    expect(app.title).toEqual('vividi');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vividi-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, vividi');
   });
 });
--- vividi/src/app/app.component.ts
+++ vividi/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'vividi-frontend';
+  title = 'vividi';
 }
