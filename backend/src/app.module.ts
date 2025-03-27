9f61437308abaf2681d72b4e86b7c34ffb4389e3
xprss <vincenzo.sagristano@yahoo.com>
2025-03-27 22:32:04 +0100

    added connection to the mongo instance

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { VibeModule } from './vibe/vibe.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [VibeModule, UserModule],
+  imports: [
+    VibeModule,
+    UserModule,
+    MongooseModule.forRoot(
+      'mongodb://root:example@mongodb:27017/vividi?authSource=admin',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
