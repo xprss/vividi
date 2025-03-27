c1dd2731883347fabb5b1e67555fc66e43fb67dd
xprss <vincenzo.sagristano@yahoo.com>
2025-03-27 21:06:11 +0100

    created: user resource

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,14 +1,34 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
-import { User } from "./user.dto";
-import { UserService } from "./user.service";
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller("user")
+@Controller('user')
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private readonly userService: UserService) {}
 
-    @Get()
-    @HttpCode(200)
-    public async getUser(): Promise<User[]> {
-        return this.userService.getUsers();
-    }
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from "@nestjs/common";
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from "mongoose";
-import { User } from "./user.dto";
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
 
-    public async getUsers(): Promise<User[]> {
-        return this.userModel.find().exec();
-    }
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
