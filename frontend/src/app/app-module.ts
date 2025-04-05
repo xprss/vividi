b4f159d18006448b13cd9079d173b53f86641f00
xprss <vincenzo.sagristano@yahoo.com>
2025-04-05 04:19:20 +0200

    updated app with new components

---
+++ frontend/src/app/app-module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { LayoutsModule } from './layouts/layouts.module';
+
+@NgModule({
+  declarations: [],
+  imports: [LayoutsModule],
+})
+export class AppModule {}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,36 +1,11 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { MessageModule } from 'primeng/message';
 import { CommonModule } from '@angular/common';
-import { UserPostComponent } from './components/user-post/user-post.component';
-import { Avatar } from 'primeng/avatar';
-
+import { LayoutsModule } from './layouts/layouts.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, InputTextModule, ButtonModule, MessageModule, Avatar, FormsModule, UserPostComponent],
+  imports: [CommonModule, LayoutsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  public posts: any[] = [
-    {
-      username: "Vincenzo Sagristano",
-      timestamp: "8 novembre 2025",
-      description: "ciao raga auguriii"
-    },
-    {
-      username: "Noemi Mosca",
-      timestamp: "8 novembre 2025",
-      description: "Mi piacciono i colori!!!"
-    },
-    {
-      username: "Luca Sagristano",
-      timestamp: "8 novembre 2025",
-      description: "Nn ho capit. loris mangi.a.casa"
-    },
-  ];
-}
+export class AppComponent {}
