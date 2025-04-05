c64bb08291b3b1db3fed670618ee253e6b4fe33c
xprss <vincenzo.sagristano@yahoo.com>
2025-04-05 04:18:09 +0200

    shared module to export base page, header and navbar

---
+++ frontend/src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BasePageComponent } from './components/base-page/base-page.component';
+
+@NgModule({
+  declarations: [],
+  imports: [BasePageComponent],
+  exports: [BasePageComponent]
+})
+export class SharedModule {}
