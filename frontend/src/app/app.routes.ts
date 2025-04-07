5fed22503d835b60cbafc72abc4b6b0f5bc9bd90
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 14:06:28 +0200

    added a new route for the new vibe page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './layouts/home/home.component';
 import { UnavailableComponent } from './layouts/unavailable/unavailable.component';
+import { NewVibeComponent } from './layouts/new-vibe/new-vibe.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'newVibe',
+    component: NewVibeComponent,
   },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
