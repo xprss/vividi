0cbc0fd7e93edbe88af5d6578e545f698c1e6ca3
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 13:55:43 +0200

    added routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutsModule } from './layouts/layouts.module';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LayoutsModule],
+  imports: [CommonModule, LayoutsModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- frontend/src/app/layouts/layouts.module.ts
+++ frontend/src/app/layouts/layouts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { UnavailableComponent } from './unavailable/unavailable.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HomeComponent],
+  imports: [CommonModule, HomeComponent, UnavailableComponent],
   exports: [HomeComponent],
 })
 export class LayoutsModule {}
