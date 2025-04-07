fc61ea01657c52d3bd01e18753730fde3a3e71a5
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 19:45:54 +0200

    404 text updated

--- frontend/src/app/layouts/unavailable/unavailable.component.ts
+++ frontend/src/app/layouts/unavailable/unavailable.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BasePageComponent } from '../../shared/components/base-page/base-page.component';
 import { Button } from 'primeng/button';
 import { Router } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
 
 @Component({
   selector: 'v2d-unavailable',
   standalone: true,
-  imports: [BasePageComponent, Button],
+  imports: [SharedModule, Button],
   templateUrl: './unavailable.component.html',
   styleUrl: './unavailable.component.scss',
 })
