1ca244cc4a895013d31c6572c2666fb2bf2557b7
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 14:05:47 +0200

    added a new component for the new vibe page

---
+++ frontend/src/app/layouts/new-vibe/new-vibe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BasePageComponent } from "../../shared/components/base-page/base-page.component";
+
+@Component({
+  selector: 'v2d-new-vibe',
+  standalone: true,
+  imports: [BasePageComponent],
+  templateUrl: './new-vibe.component.html',
+  styleUrl: './new-vibe.component.scss'
+})
+export class NewVibeComponent {
+
+}
