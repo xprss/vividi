93f5b9982066df7ec2b2f1d435d951734833a211
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 13:55:18 +0200

    added a new component for the not found page

---
+++ frontend/src/app/layouts/unavailable/unavailable.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'v2d-unavailable',
+  standalone: true,
+  imports: [],
+  templateUrl: './unavailable.component.html',
+  styleUrl: './unavailable.component.scss'
+})
+export class UnavailableComponent {
+
+}
