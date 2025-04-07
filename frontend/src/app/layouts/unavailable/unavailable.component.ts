f4c3b8ebefecd96b627fc2779d00f50d7d176ea0
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 14:06:03 +0200

    unavailable page has a cta to go back to the home page

--- frontend/src/app/layouts/unavailable/unavailable.component.ts
+++ frontend/src/app/layouts/unavailable/unavailable.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { BasePageComponent } from '../../shared/components/base-page/base-page.component';
+import { Button } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'v2d-unavailable',
   standalone: true,
-  imports: [],
+  imports: [BasePageComponent, Button],
   templateUrl: './unavailable.component.html',
-  styleUrl: './unavailable.component.scss'
+  styleUrl: './unavailable.component.scss',
 })
 export class UnavailableComponent {
+  constructor(private readonly router: Router) {}
 
+  protected navigateToHomePage() {
+    this.router.navigate(['/home']);
+  }
 }
