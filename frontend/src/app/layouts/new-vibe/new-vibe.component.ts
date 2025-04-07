b46dd9afd8c90e521e9baf7dc9520b17a970ed4d
xprss <vincenzo.sagristano@yahoo.com>
2025-04-07 19:46:32 +0200

    new vibe component has a more defined title

--- frontend/src/app/layouts/new-vibe/new-vibe.component.ts
+++ frontend/src/app/layouts/new-vibe/new-vibe.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { BasePageComponent } from "../../shared/components/base-page/base-page.component";
+import { BasePageComponent } from '../../shared/components/base-page/base-page.component';
+import { SharedModule } from '../../shared/shared.module';
+import { PageTitleComponent } from '../../shared/components/page-title/page-title.component';
 
 @Component({
   selector: 'v2d-new-vibe',
   standalone: true,
-  imports: [BasePageComponent],
+  imports: [SharedModule],
   templateUrl: './new-vibe.component.html',
-  styleUrl: './new-vibe.component.scss'
+  styleUrl: './new-vibe.component.scss',
 })
-export class NewVibeComponent {
-
-}
+export class NewVibeComponent {}
