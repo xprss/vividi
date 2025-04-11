e94e8b3cbdbcd8ffb017e58779d5a3ede8be7b92
xprss <vincenzo.sagristano@yahoo.com>
2025-04-11 20:42:10 +0200

    tentatives - no good

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { IftaLabelModule } from 'primeng/iftalabel';
 import { LayoutsModule } from './layouts/layouts.module';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, LayoutsModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [
+    LayoutsModule,
+    CommonModule,
+    IftaLabelModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    IftaLabelModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
