4f35604b0f7a1084c90c751a4232040cf8c9f3ef
xprss <vincenzo.sagristano@yahoo.com>
2025-04-11 13:28:51 +0200

    added FeaturesModule to import to have components for the new vibe form

--- frontend/src/app/layouts/new-vibe/new-vibe.component.ts
+++ frontend/src/app/layouts/new-vibe/new-vibe.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BasePageComponent } from '../../shared/components/base-page/base-page.component';
 import { SharedModule } from '../../shared/shared.module';
 import { PageTitleComponent } from '../../shared/components/page-title/page-title.component';
+import { FeaturesModule } from '../../features/features.module';
 
 @Component({
   selector: 'v2d-new-vibe',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, FeaturesModule],
   templateUrl: './new-vibe.component.html',
   styleUrl: './new-vibe.component.scss',
 })
