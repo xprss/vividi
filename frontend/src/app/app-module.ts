20ea93f7fda816904b05849f3db7c1169e73f9dd
xprss <vincenzo.sagristano@yahoo.com>
2025-04-11 20:41:45 +0200

    this is not useful for now

--- frontend/src/app/app-module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LayoutsModule } from './layouts/layouts.module';
-
-@NgModule({
-  declarations: [],
-  imports: [LayoutsModule],
-})
-export class AppModule {}
