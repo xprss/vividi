35bf89be292d857f0627cb4ee41817ed1ad17b1d
xprss <vincenzo.sagristano@yahoo.com>
2025-04-12 11:23:57 +0200

    added button to toggle the info center

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { DrawerService } from '../../../core/drawer.service';
 
 @Component({
   selector: 'v2d-header',
   standalone: true,
-  imports: [AvatarModule],
+  imports: [AvatarModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(protected readonly drawerService: DrawerService) {}
+}
