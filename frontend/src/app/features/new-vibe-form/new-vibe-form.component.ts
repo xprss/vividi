ebbb7031bafc10f06825b243ee57c700a6132bb8
xprss <vincenzo.sagristano@yahoo.com>
2025-04-12 11:24:08 +0200

    drafted new vibe page

--- frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
+++ frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
@@ -2,10 +2,22 @@ import { Component } from '@angular/core';
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'v2d-new-vibe-form',
-  imports: [IftaLabelModule, FormsModule, InputTextModule],
+  imports: [
+    CommonModule,
+    IftaLabelModule,
+    FormsModule,
+    InputTextModule,
+    TextareaModule,
+    FloatLabelModule,
+    ChipModule,
+  ],
   templateUrl: './new-vibe-form.component.html',
   styleUrl: './new-vibe-form.component.scss',
 })
