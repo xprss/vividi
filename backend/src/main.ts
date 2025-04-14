e04e571b7ded7cf3ff7a8e1d00e3e8614f9a4018
xprss <vincenzo.sagristano@yahoo.com>
2025-04-14 22:04:52 +0200

    allowed CORS for the communication with frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: false,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Vividi')
     .setDescription(
