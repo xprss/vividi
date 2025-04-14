621e6e724303bbabb673794d2ff48ec4fb9719ed
xprss <vincenzo.sagristano@yahoo.com>
2025-04-14 22:06:07 +0200

    drafted for to create a new vibe and store it in the real database

--- frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
+++ frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
@@ -6,6 +6,8 @@ import { TextareaModule } from 'primeng/textarea';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CommonModule } from '@angular/common';
 import { ChipModule } from 'primeng/chip';
+import { ServerService } from '../../core/server.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'v2d-new-vibe-form',
@@ -17,10 +19,26 @@ import { ChipModule } from 'primeng/chip';
     TextareaModule,
     FloatLabelModule,
     ChipModule,
+    ButtonModule
   ],
   templateUrl: './new-vibe-form.component.html',
   styleUrl: './new-vibe-form.component.scss',
 })
 export class NewVibeFormComponent {
+  constructor(private readonly serverService: ServerService) {}
+
+  protected user: string = '';
   protected description: string = '';
+  protected moment: string = '';
+
+  public setMoment(moment: string) {
+    this.moment = moment;
+  }
+
+  public submit(): void {
+    const body: any = {user: this.user, description: this.description, moment: this.moment};
+    console.log(body);
+    
+    this.serverService.postVibe(body);
+  }
 }
