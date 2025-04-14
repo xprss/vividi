f6f1ef9b584dbdcab932c699e9a5a14f63015e6d
xprss <vincenzo.sagristano@yahoo.com>
2025-04-14 22:05:19 +0200

    vibes are now fetched!

--- frontend/src/app/layouts/home/home.component.ts
+++ frontend/src/app/layouts/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { FeaturesModule } from '../../features/features.module';
+import { ServerService } from '../../core/server.service';
 
 @Component({
   selector: 'v2d-home',
@@ -9,4 +10,14 @@ import { FeaturesModule } from '../../features/features.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private readonly serverService: ServerService) {}
+
+  protected vibes: {user: string, description: string, moment: string}[] = [];
+
+  public ngOnInit(): void {
+    this.serverService.getAllVibes().then((data) => {
+      this.vibes = data;
+    });
+  }
+}
