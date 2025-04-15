b27be27d820a534278151466a2d1ac583f2bad69
xprss <vincenzo.sagristano@yahoo.com>
2025-04-15 14:21:04 +0200

    moment has now also human readable translations

--- backend/libs/common/moment.enum.ts
+++ backend/libs/common/moment.enum.ts
@@ -4,3 +4,10 @@ export enum Moment {
   LUNCH = 'LUNCH',
   FOLLOWING_DAYS = 'FOLLOWING_DAYS',
 }
+
+export const MomentLabels: Record<Moment, string> = {
+  [Moment.PREVIOUS_DAYS]: 'Giorni precedenti',
+  [Moment.CEREMONY]: 'Cerimonia',
+  [Moment.LUNCH]: 'Ricevimento',
+  [Moment.FOLLOWING_DAYS]: 'Giorni successivi',
+};
