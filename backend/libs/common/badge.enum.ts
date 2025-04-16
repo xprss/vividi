b70376fc9d399d54734a6695d4747f42de0ddee6
xprss <80033167+xprss@users.noreply.github.com>
2025-04-16 14:45:28 +0200

    Update badge.enum.ts

--- backend/libs/common/badge.enum.ts
+++ backend/libs/common/badge.enum.ts
@@ -1,9 +1,27 @@
 export enum Badge {
   BRIDE = 'BRIDE',
   GROOM = 'GROOM',
+  BESTMAN = 'BESTMAN',
+  BRIDESMAID = 'BRIDESMAID',
+  PARENT = 'PARENT',
+  DEVELOPER = 'DEVELOPER',
+  QUIZ_FIRST_CLASSIFIED = 'QUIZ_FIRST_CLASSIFIED',
+  QUIZ_SECOND_CLASSIFIED = 'QUIZ_SECOND_CLASSIFIED',
+  QUIZ_THIRD_CLASSIFIED = 'QUIZ_THIRD_CLASSIFIED',
+  QUIZ_LAST_CLASSIFIED = 'QUIZ_LAST_CLASSIFIED',
+  CAT_FINDER = 'CAT_FINDER'
 }
 
 export const BadgeLabels: Record<Badge, string> = {
   [Badge.BRIDE]: 'Sposa',
   [Badge.GROOM]: 'Sposo',
+  [Badge.BESTMAN]: 'Testimone',
+  [Badge.BRIDESMAID]: 'Damigella',
+  [Badge.PARENT]: 'Genitore',
+  [Badge.DEVELOPER]: 'Sviluppatore',
+  [Badge.QUIZ_FIRST_CLASSIFIED]: 'Primo posto al quiz',
+  [Badge.QUIZ_SECOND_CLASSIFIED]: 'Secondo posto al quiz',
+  [Badge.QUIZ_THIRD_CLASSIFIED]: 'Terzo posto al quiz',
+  [Badge.QUIZ_LAST_CLASSIFIED]: 'Ultimo posto al quiz',
+  [Badge.CAT_FINDER]: 'Cacciatore di gatti'
 };
