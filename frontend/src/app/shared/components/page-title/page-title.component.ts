19abce6900ffc311d0a9679ceccd7413237d4c61
xprss <vincenzo.sagristano@yahoo.com>
2025-04-23 16:33:11 +0200

    the page title can be centered

--- frontend/src/app/shared/components/page-title/page-title.component.ts
+++ frontend/src/app/shared/components/page-title/page-title.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'v2d-page-title',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './page-title.component.html',
   styleUrl: './page-title.component.scss',
 })
 export class PageTitleComponent {
   @Input() public title: string = "";
+  @Input() public centered: boolean = false;
 }
