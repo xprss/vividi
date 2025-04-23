214771564e8e682a089b09212b1bd14a83df35b4
xprss <vincenzo.sagristano@yahoo.com>
2025-04-23 16:32:58 +0200

    defined custom theme named Wedding

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import { WeddingPreset } from './themes/Wedding';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({ 
       theme: {
-        preset: Aura
+        preset: WeddingPreset
       }
     })
   ]
---
+++ frontend/src/app/themes/Wedding.ts
@@ -0,0 +1,30 @@
+import { definePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+
+export const WeddingPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{amber.50}',
+      100: '{amber.100}',
+      200: '{amber.200}',
+      300: '{amber.300}',
+      400: '{amber.400}',
+      500: '{amber.500}',
+      600: '{amber.600}',
+      700: '{amber.700}',
+      800: '{amber.800}',
+      900: '{amber.900}',
+      950: '{amber.950}',
+    },
+    root: {
+        surfaceGround: '{pink.500}'
+    }
+  },
+  colorScheme: {
+    light: {
+        root: {
+            background: '{pink.500}'
+        }
+    }
+  }
+});
