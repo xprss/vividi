def600b7a02e17505fe8c00ea03d42f08e85c395
xprss <vincenzo.sagristano@yahoo.com>
2025-04-23 16:32:33 +0200

    added connection towards firebase

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { FirebaseApp, initializeApp } from '@firebase/app';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const firebaseApp: FirebaseApp = initializeApp({
+  apiKey: environment.apiKey,
+  authDomain: environment.authDomain,
+  projectId: environment.projectId,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+  appId: environment.appId,
+});
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
