4f0f890be4e04903b92cb190d434790cd4c4dae7
xprss <vincenzo.sagristano@yahoo.com>
2025-04-23 16:32:06 +0200

    added keys and needed info to connect to firebase

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,4 +2,10 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8000/api',
+  apiKey: 'AIzaSyCk8Epfaw0qVXg9EuW-mmLE6ZY_ZdZQ1R0',
+  authDomain: 'vividivibes.firebaseapp.com',
+  projectId: 'vividivibes',
+  storageBucket: 'vividivibes.firebasestorage.app',
+  messagingSenderId: '569465452672',
+  appId: '1:569465452672:web:a9d48095e5122f6537ce65',
 };
