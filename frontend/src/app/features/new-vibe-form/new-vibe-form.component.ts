688ba5e48607e30d84ce5211ec9ce3d5ae9522d5
xprss <vincenzo.sagristano@yahoo.com>
2025-04-24 11:54:00 +0200

    improved new vibe creation

--- frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
+++ frontend/src/app/features/new-vibe-form/new-vibe-form.component.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { ChipModule } from 'primeng/chip';
 import { ServerService } from '../../core/server.service';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'v2d-new-vibe-form',
@@ -19,26 +20,31 @@ import { ButtonModule } from 'primeng/button';
     TextareaModule,
     FloatLabelModule,
     ChipModule,
-    ButtonModule
+    ButtonModule,
   ],
   templateUrl: './new-vibe-form.component.html',
   styleUrl: './new-vibe-form.component.scss',
 })
 export class NewVibeFormComponent {
-  constructor(private readonly serverService: ServerService) {}
+  constructor(
+    private readonly serverService: ServerService,
+    protected readonly authService: AuthService
+  ) {}
 
   protected user: string = '';
   protected description: string = '';
-  protected moment: string = '';
-
-  public setMoment(moment: string) {
-    this.moment = moment;
-  }
+  protected moment: string = 'CEREMONY';
 
   public submit(): void {
-    const body: any = {user: this.user, description: this.description, moment: this.moment};
-    console.log(body);
-    
+    const body: any = {
+      userId: this.authService.getUser()?.uid,
+      userFullName: this.authService.getUser()?.displayName,
+      description: this.description,
+      moment: this.moment,
+      creationTimestamp: Date.now(),
+      pictureRef: 'vibes/vibe2.jpg',
+    };
+
     this.serverService.postVibe(body);
   }
 }
