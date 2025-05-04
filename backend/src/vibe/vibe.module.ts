2911eab76dd79d346e6432fb00bff69768e01f9a
xprss <vincenzo.sagristano@yahoo.com>
2025-05-04 16:31:57 +0200

    imported google service

--- backend/src/vibe/vibe.module.ts
+++ backend/src/vibe/vibe.module.ts
@@ -3,10 +3,12 @@ import { VibeService } from './vibe.service';
 import { VibeController } from './vibe.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Vibe, VibeSchema } from './schemas/vibe.schema';
+import { GoogleAPIModule } from 'src/google/google-api.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Vibe.name, schema: VibeSchema }]),
+    GoogleAPIModule,
   ],
   controllers: [VibeController],
   providers: [VibeService],
