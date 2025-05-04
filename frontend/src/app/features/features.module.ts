db067d09204dde471e93b116ee5a7ac44aa47c7f
xprss <vincenzo.sagristano@yahoo.com>
2025-05-04 17:59:19 +0200

    needed for the file picker, might remove idk actually should adopt http client sooner or later

--- frontend/src/app/features/features.module.ts
+++ frontend/src/app/features/features.module.ts
@@ -5,8 +5,10 @@ import { NewVibeFormComponent } from './new-vibe-form/new-vibe-form.component';
 import { NewVibeImageUploadComponent } from './new-vibe-image-upload/new-vibe-image-upload.component';
 import { NewVibeReviewComponent } from './new-vibe-review/new-vibe-review.component';
 import { NewVibeTagsComponent } from './new-vibe-tags/new-vibe-tags.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
+  providers: [provideHttpClient()],
   declarations: [],
   imports: [
     CommonModule,
