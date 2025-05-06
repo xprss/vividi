ef6e42d340f2f0f1c32c7a8f1cf36c0f3fa9eec3
xprss <vincenzo.sagristano@yahoo.com>
2025-05-06 19:34:30 +0200

    added progress spin to the dialog component

--- frontend/src/app/shared/components/dialog/dialog.component.ts
+++ frontend/src/app/shared/components/dialog/dialog.component.ts
@@ -4,10 +4,11 @@ import { DialogModule } from 'primeng/dialog';
 import { DialogService } from '../../../core/dialog.service';
 import { ButtonModule } from 'primeng/button';
 import { NavbarService } from '../../../core/navbar.service';
+import { ProgressSpinner } from 'primeng/progressspinner';
 
 @Component({
   selector: 'v2d-dialog',
-  imports: [DialogModule, CommonModule, ButtonModule],
+  imports: [DialogModule, CommonModule, ButtonModule, ProgressSpinner],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss',
 })
