952ea3909b55663409d0c2dd7fa1bb12f01cd539
xprss <vincenzo.sagristano@yahoo.com>
2025-05-06 19:35:17 +0200

    adopted ctas in user page

--- frontend/src/app/layouts/user-page/user-page.component.ts
+++ frontend/src/app/layouts/user-page/user-page.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthService } from '../../core/auth.service';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { CtasComponent } from '../../shared/components/ctas/ctas.component';
+import { CtasService } from '../../core/ctas.service';
 
 @Component({
   selector: 'v2d-user-page',
   standalone: true,
-  imports: [SharedModule, ButtonModule],
+  imports: [SharedModule, ButtonModule, CommonModule, CtasComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss',
 })
-export class UserPageComponent {
-  constructor(protected readonly authService: AuthService) {}
+export class UserPageComponent implements OnInit {
+  constructor(
+    protected readonly authService: AuthService,
+    protected readonly ctasService: CtasService
+  ) {}
+  public ngOnInit(): void {
+    this.ctasService.showCtas([
+      {
+        label: 'Esci da Vividi',
+        icon: 'pi pi-home',
+        severity: 'danger',
+        disabled: false,
+        action: () => this.authService.signOut(),
+      },
+    ]);
+  }
 }
