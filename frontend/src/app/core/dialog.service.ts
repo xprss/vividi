c323bf583cb25f4ff963c2d676e7af74137ff5b1
xprss <vincenzo.sagristano@yahoo.com>
2025-05-06 19:36:48 +0200

    loading + hiding

--- frontend/src/app/core/dialog.service.ts
+++ frontend/src/app/core/dialog.service.ts
@@ -8,14 +8,34 @@ export class DialogService {
   constructor() { }
 
   public isDialogOpen: boolean = false;
+  public isClosable: boolean = true;
   public header: string = '';
   public body: string = '';
   public buttons: DialogButton[] = [];
+  public isLoading: boolean = false;
 
   public showDialog(header: string, body: string, buttons: DialogButton[]) {
+    this.isClosable = true;
+    this.isLoading = false;
     this.header = header;
     this.body = body;
     this.buttons = buttons;
     this.isDialogOpen = true;
+  }
+
+  public hideDialog() {
+    this.header = '';
+    this.body = '';
+    this.buttons = [];
+    this.isClosable = true;
+    this.isDialogOpen = false;
+  }
+
+
+  public showLoadingDialog() {
+    this.header = 'Caricamento in corso...';
+    this.isLoading = true;
+    this.isDialogOpen = true;
+    this.isClosable = false;
   }
 }
