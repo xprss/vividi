5c3f5f6073541ac694a12826cb4b6cc04968b91b
xprss <vincenzo.sagristano@yahoo.com>
2025-05-06 19:35:24 +0200

    cencio

--- frontend/src/app/features/features.module.ts
+++ frontend/src/app/features/features.module.ts
@@ -2,28 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VibePostComponent } from './vibe-post/vibe-post.component';
 import { NewVibeFormComponent } from './new-vibe-form/new-vibe-form.component';
-import { NewVibeImageUploadComponent } from './new-vibe-image-upload/new-vibe-image-upload.component';
-import { NewVibeReviewComponent } from './new-vibe-review/new-vibe-review.component';
-import { NewVibeTagsComponent } from './new-vibe-tags/new-vibe-tags.component';
 import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   providers: [provideHttpClient()],
   declarations: [],
-  imports: [
-    CommonModule,
-    VibePostComponent,
-    NewVibeFormComponent,
-    NewVibeImageUploadComponent,
-    NewVibeReviewComponent,
-    NewVibeTagsComponent,
-  ],
-  exports: [
-    VibePostComponent,
-    NewVibeFormComponent,
-    NewVibeImageUploadComponent,
-    NewVibeReviewComponent,
-    NewVibeTagsComponent,
-  ],
+  imports: [CommonModule, VibePostComponent, NewVibeFormComponent],
+  exports: [VibePostComponent, NewVibeFormComponent],
 })
 export class FeaturesModule {}
