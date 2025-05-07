eead2bff2249667e76b2d8ce735b54799898352d
xprss <vincenzo.sagristano@yahoo.com>
2025-05-07 21:20:46 +0200

    added horrible methods to create and retrieve accounts - will change adopting httpclient and refining backend too now it's just too late and i deserve bit of rest

--- frontend/src/app/core/server.service.ts
+++ frontend/src/app/core/server.service.ts
@@ -7,7 +7,7 @@ import { environment } from '../../environments/environment';
 export class ServerService {
   constructor() {}
 
-  async getAllVibes(): Promise<
+  public async getAllVibes(): Promise<
     { user: string; description: string; moment: string }[]
   > {
     const response = await fetch(environment.apiBaseUrl + '/vibe/', {
@@ -25,7 +25,7 @@ export class ServerService {
     return response;
   }
 
-  async postVibe(data: any): Promise<any> {
+  public async postVibe(data: any): Promise<any> {
     const formData = new FormData();
     formData.append('file', data.file);
     formData.append('userId', data.userId);
@@ -39,7 +39,39 @@ export class ServerService {
     return response.json();
   }
 
-  async deleteVibe(vibeId: string): Promise<void> {
+  public async createAccount(
+    firstName: string,
+    lastName: string,
+    email: string
+  ): Promise<Response> {
+    const body: any = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+
+    const response: Response = await fetch(environment.apiBaseUrl + '/user/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    return response;
+  }
+
+  public async getAccount(email: string): Promise<any> {
+    const response = await fetch(environment.apiBaseUrl + '/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({email: email})
+    });
+    return response;
+  }
+
+  public async deleteVibe(vibeId: string): Promise<void> {
     const response = await fetch(environment.apiBaseUrl + '/vibe/' + vibeId, {
       method: 'DELETE',
       headers: {
