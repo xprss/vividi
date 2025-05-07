a269ec732b0ac571a1fbde412a66161d4a489628
xprss <vincenzo.sagristano@yahoo.com>
2025-05-07 21:19:32 +0200

    added signon component

--- frontend/src/app/layouts/layouts.module.ts
+++ frontend/src/app/layouts/layouts.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { UnavailableComponent } from './unavailable/unavailable.component';
+import { SignOnComponent } from './sign-on/sign-on.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HomeComponent, UnavailableComponent],
+  imports: [CommonModule, HomeComponent, UnavailableComponent, SignOnComponent],
   exports: [HomeComponent],
 })
 export class LayoutsModule {}
