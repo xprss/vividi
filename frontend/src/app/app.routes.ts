6d4fab02e2dc6d394a7a6a000169c9ca215dd860
xprss <vincenzo.sagristano@yahoo.com>
2025-05-07 21:15:54 +0200

    added routes for signin, signon, forgot-password

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,22 +3,51 @@ import { HomeComponent } from './layouts/home/home.component';
 import { UnavailableComponent } from './layouts/unavailable/unavailable.component';
 import { NewVibeComponent } from './layouts/new-vibe/new-vibe.component';
 import { UserPageComponent } from './layouts/user-page/user-page.component';
+import { SignOnComponent } from './layouts/sign-on/sign-on.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthPageComponent } from './layouts/auth-page/auth-page.component';
+import { NoAuthGuard } from './guards/no-auth.guard';
+import { ForgotPasswordComponent } from './layouts/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'newVibe',
     component: NewVibeComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'me',
     component: UserPageComponent,
+    canActivate: [AuthGuard],
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
-    path: '**', component: UnavailableComponent
-  }
+    path: 'signin',
+    component: AuthPageComponent,
+    canActivate: [NoAuthGuard],
+  },
+  {
+    path: 'signon',
+    component: SignOnComponent,
+    canActivate: [NoAuthGuard],
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+    canActivate: [NoAuthGuard],
+  },
+  { 
+    path: '', 
+    redirectTo: '/home', 
+    pathMatch: 'full' 
+  },
+  {
+    path: '**',
+    component: UnavailableComponent,
+    canActivate: [AuthGuard],
+  },
 ];
