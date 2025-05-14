8d9bd5ee87989f99b49f0c00e13a51585cc9b2a1
xprss <vincenzo.sagristano@yahoo.com>
2025-05-14 18:30:51 +0200

    ooops

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,6 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { FirebaseApp, initializeApp } from '@firebase/app';
 import { environment } from './environments/environment';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const firebaseApp: FirebaseApp = initializeApp({
   apiKey: environment.apiKey,
@@ -14,15 +13,6 @@ const firebaseApp: FirebaseApp = initializeApp({
   appId: environment.appId,
 });
 
-bootstrapApplication(AppComponent, appConfig)
-  .then((app) => {
-    const config = new DocumentBuilder()
-      .setTitle('V2D-API')
-      .setDescription('API documentation')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
-  })
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
