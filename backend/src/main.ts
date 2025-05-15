7ef865ba06336821a88329de9ce9f1588e50fbbe
xprss <vincenzo.sagristano@yahoo.com>
2025-05-16 01:01:58 +0200

    logger enabled on backedn

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
 
   app.enableCors({
     origin: 'http://localhost:4200',
