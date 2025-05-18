58d6bcaee350c3e0de1ea54fbd45a2acb6bb0c28
xprss <vincenzo.sagristano@yahoo.com>
2025-05-18 18:48:01 +0200

    added end of feed message

---
+++ frontend/src/app/features/end-of-feed/end-of-feed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'v2d-end-of-feed',
+  imports: [],
+  templateUrl: './end-of-feed.component.html',
+  styleUrl: './end-of-feed.component.scss'
+})
+export class EndOfFeedComponent {
+
+}
--- frontend/src/app/features/features.module.ts
+++ frontend/src/app/features/features.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { VibePostComponent } from './vibe-post/vibe-post.component';
 import { NewVibeFormComponent } from './new-vibe-form/new-vibe-form.component';
 import { provideHttpClient } from '@angular/common/http';
+import { EndOfFeedComponent } from './end-of-feed/end-of-feed.component';
 
 @NgModule({
   providers: [provideHttpClient()],
   declarations: [],
-  imports: [CommonModule, VibePostComponent, NewVibeFormComponent],
-  exports: [VibePostComponent, NewVibeFormComponent],
+  imports: [CommonModule, VibePostComponent, NewVibeFormComponent, EndOfFeedComponent],
+  exports: [VibePostComponent, NewVibeFormComponent, EndOfFeedComponent],
 })
 export class FeaturesModule {}
