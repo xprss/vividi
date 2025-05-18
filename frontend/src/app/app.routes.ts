a1baf4f3c943f994d3ad5e4b4f8640319c6b4c64
xprss <vincenzo.sagristano@yahoo.com>
2025-05-18 20:13:04 +0200

    added single vibe page route + shared css

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { AuthPageComponent } from './layouts/auth-page/auth-page.component';
 import { NoAuthGuard } from './guards/no-auth.guard';
 import { ForgotPasswordComponent } from './layouts/forgot-password/forgot-password.component';
+import { SingleVibePageComponent } from './layouts/single-vibe-page/single-vibe-page.component';
 
 export const routes: Routes = [
   {
@@ -23,6 +24,11 @@ export const routes: Routes = [
   {
     path: 'me',
     component: UserPageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'vibe/:vibeId',
+    component: SingleVibePageComponent,
     canActivate: [AuthGuard],
   },
   {
