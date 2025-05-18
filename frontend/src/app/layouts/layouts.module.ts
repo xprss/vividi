650a1cd41a7e8105e2eba75df089c639552552b5
xprss <vincenzo.sagristano@yahoo.com>
2025-05-18 20:14:02 +0200

    added SingleVibePageComponent in layout module

--- frontend/src/app/layouts/layouts.module.ts
+++ frontend/src/app/layouts/layouts.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { UnavailableComponent } from './unavailable/unavailable.component';
 import { SignOnComponent } from './sign-on/sign-on.component';
+import { SingleVibePageComponent } from './single-vibe-page/single-vibe-page.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, HomeComponent, UnavailableComponent, SignOnComponent],
+  imports: [CommonModule, HomeComponent, UnavailableComponent, SignOnComponent, SingleVibePageComponent],
   exports: [HomeComponent],
 })
 export class LayoutsModule {}
