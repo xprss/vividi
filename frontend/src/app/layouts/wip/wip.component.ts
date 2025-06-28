755c485e1ab77c97a7a5ec60c9523fd1da447dc8
xprss <vincenzo.sagristano@yahoo.com>
2025-06-28 16:25:27 +0200

    wip page has been enriched

--- frontend/src/app/layouts/wip/wip.component.ts
+++ frontend/src/app/layouts/wip/wip.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { Button } from 'primeng/button';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'v2d-wip',
   standalone: true,
-  imports: [SharedModule, HeaderComponent],  
+  imports: [SharedModule, Button],  
   templateUrl: './wip.component.html',
   styleUrl: './wip.component.scss'
 })
 export class WipComponent {
-
+  protected openGithub(): void {
+    window.location.href = 'https://www.github.com/xprss/vividi';
+  }
 }
