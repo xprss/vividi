b188e87be4198c5a8da74e0c14c65c337b0169f8
xprss <vincenzo.sagristano@yahoo.com>
2025-06-30 22:08:39 +0200

    added show more button to see dialog working on mobile

--- frontend/src/app/layouts/wip/wip.component.ts
+++ frontend/src/app/layouts/wip/wip.component.ts
@@ -1,17 +1,23 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { Button } from 'primeng/button';
+import { DialogService } from 'src/app/core/dialog.service';
+import { DialogComponent } from 'src/app/shared/components/dialog/dialog.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'v2d-wip',
   standalone: true,
-  imports: [SharedModule, Button],  
+  imports: [SharedModule, Button, DialogComponent],
   templateUrl: './wip.component.html',
-  styleUrl: './wip.component.scss'
+  styleUrl: './wip.component.scss',
 })
 export class WipComponent implements AfterViewInit {
+  constructor(private readonly dialogService: DialogService) {}
+
   public ngAfterViewInit(): void {
-    const video: HTMLVideoElement | null = document.getElementById('wipVideo') as HTMLVideoElement;
+    const video: HTMLVideoElement | null = document.getElementById(
+      'wipVideo'
+    ) as HTMLVideoElement;
     if (!video) {
       return;
     }
@@ -28,5 +34,20 @@ export class WipComponent implements AfterViewInit {
 
   protected openGithub(): void {
     window.location.href = 'https://www.github.com/xprss/vividi';
+  }
+
+  protected showMore(): void {
+    this.dialogService.showDialog(
+      'ottonovembre.it sta arrivando...',
+      "La webapp ufficiale del matrimonio dell'anno sta per sbarcare sul web. Tenetevi pronti.",
+      [
+        {
+          label: 'Chiudi',
+          icon: 'pi pi-times',
+          severity: 'secondary',
+          action: () => this.dialogService.hideDialog(),
+        },
+      ]
+    );
   }
 }
