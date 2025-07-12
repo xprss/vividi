77a5ef18f9ea398396cb35488af739e9755f7db3
xprss <vincenzo.sagristano@yahoo.com>
2025-07-12 04:30:49 +0000

    setting env to prod

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ import { max } from "rxjs";
 
 // environment.ts (for development)
 export const environment = {
-  production: false,
+  production: true,
   apiBaseUrl: 'http://localhost:8000',
   apiKey: 'AIzaSyCk8Epfaw0qVXg9EuW-mmLE6ZY_ZdZQ1R0',
   authDomain: 'vividivibes.firebaseapp.com',
