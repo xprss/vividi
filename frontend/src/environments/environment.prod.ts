e72086ffdf61bedbaf1147d1b4cc7155d10b3c37
xprss <vincenzo.sagristano@yahoo.com>
2025-07-20 19:06:57 +0200

    added production env file

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+import { max } from "rxjs";
+
+// environment.ts (for development)
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://server:8000',
+  apiKey: 'AIzaSyCk8Epfaw0qVXg9EuW-mmLE6ZY_ZdZQ1R0',
+  authDomain: 'vividivibes.firebaseapp.com',
+  projectId: 'vividivibes',
+  storageBucket: 'vividivibes.firebasestorage.app',
+  messagingSenderId: '569465452672',
+  appId: '1:569465452672:web:a9d48095e5122f6537ce65',
+  maxFileSize: 7 * 1024 * 1024, // 7 MB
+};
