5cf9781c31f3785513e1982221c8290e2dcaddd0
xprss <vincenzo.sagristano@yahoo.com>
2025-08-08 17:17:23 +0000

    from amber to pink

--- frontend/src/app/themes/Wedding.ts
+++ frontend/src/app/themes/Wedding.ts
@@ -4,17 +4,17 @@ import Aura from '@primeng/themes/aura';
 export const WeddingPreset = definePreset(Aura, {
   semantic: {
     primary: {
-      50: '{amber.50}',
-      100: '{amber.100}',
-      200: '{amber.200}',
-      300: '{amber.300}',
-      400: '{amber.400}',
-      500: '{amber.500}',
-      600: '{amber.600}',
-      700: '{amber.700}',
-      800: '{amber.800}',
-      900: '{amber.900}',
-      950: '{amber.950}',
+      50: '{pink.50}',
+      100: '{pink.100}',
+      200: '{pink.200}',
+      300: '{pink.300}',
+      400: '{pink.400}',
+      500: '{pink.500}',
+      600: '{pink.600}',
+      700: '{pink.700}',
+      800: '{pink.800}',
+      900: '{pink.900}',
+      950: '{pink.950}',
     },
     root: {
         surfaceGround: '{pink.500}'
