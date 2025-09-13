eb2ae93b5d16b550ea530a0137b93eb1ea9bd4f9
xprss <vincenzo.sagristano@yahoo.com>
2025-09-13 19:05:37 +0200

    updated user schema to support new fields on frontend

--- backend/src/user/schemas/user.schema.ts
+++ backend/src/user/schemas/user.schema.ts
@@ -1,38 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Badge } from 'libs/common/badge.enum';
-import { Date } from 'mongoose';
 
 @Schema()
 export class User {
   @Prop({
-    alias: 'first_name',
+    name: 'first_name',
     required: true,
   })
   firstName: string;
 
   @Prop({
-    alias: 'last_name',
+    name: 'last_name',
     required: true,
   })
   lastName: string;
 
   @Prop({
-    alias: 'email',
     required: true,
   })
   email: string;
 
   @Prop({
-    alias: 'google_registered_at',
+    name: 'google_registered_at',
     required: true,
   })
   googleRegisteredAt: Date;
 
   @Prop({
-    alias: 'roles',
     required: false,
   })
   roles: Badge[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: (_, ret) => {
+    // Rinomina campi snake_case → camelCase
+    if (ret.first_name !== undefined) {
+      ret.firstName = ret.first_name;
+      delete ret.first_name;
+    }
+    if (ret.last_name !== undefined) {
+      ret.lastName = ret.last_name;
+      delete ret.last_name;
+    }
+    if (ret.google_registered_at !== undefined) {
+      ret.googleRegisteredAt = ret.google_registered_at;
+      delete ret.google_registered_at;
+    }
+    return ret;
+  },
+});
