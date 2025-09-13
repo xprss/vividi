e7f6232d51ffb4171bf031e0a8ef806e65490b64
xprss <vincenzo.sagristano@yahoo.com>
2025-09-13 12:43:09 +0200

    added some logging statements

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/user/schemas/user.schema';
 
 @Injectable()
 export class LoginService {
+  private readonly logger = new Logger(LoginService.name);
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async retrieveUser(email: string) {
     const users = (await this.userModel.find()).filter((user) => {
       return user.email === email;
     });
+    this.logger.log(`Found ${users.length} users with email ${email}`);
     if (users.length > 0) {
       return users[0];
     }
+    this.logger.warn(`No user found with email ${email}`);
     return null;
   }
 }
