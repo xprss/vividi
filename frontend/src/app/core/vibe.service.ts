b888d10ba83ea8b800e18d7ac9f0258744b0dbd8
xprss <vincenzo.sagristano@yahoo.com>
2025-10-25 13:14:33 +0200

    moved management of vibes to proper service

--- frontend/src/app/core/vibe.service.ts
+++ frontend/src/app/core/vibe.service.ts
@@ -2,17 +2,32 @@ import { Injectable } from '@angular/core';
 import { BaseApiService } from './baseapi.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { ServerService } from './server.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VibeService extends BaseApiService {
-  private readonly api: string = 'vibe';
+export class VibeManagementService extends BaseApiService {
+  public vibes: any[] = [];
 
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private readonly serverService: ServerService) {
     super(http, environment.apiBaseUrl);
   }
 
-  public getAll() {
+  public fetchVibes(force: boolean = false): void {
+    if (force) {
+      this.vibes = [];
+    }
+
+    if (this.vibes.length === 0) {
+      console.log('Fetching vibes from server...');
+      this.serverService.getAllVibes().subscribe((response) => {
+        this.vibes = response;
+      });
+    }
+  }
+
+  public invalidateCache(): void {
+    this.vibes = [];
   }
 }
