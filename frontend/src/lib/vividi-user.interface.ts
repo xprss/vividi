0edbf1b4bffb643c97264c967f7f7f8b86edfafc
xprss <vincenzo.sagristano@yahoo.com>
2025-11-03 23:09:05 +0100

    vividi user supports multiple badges

--- frontend/src/lib/vividi-user.interface.ts
+++ frontend/src/lib/vividi-user.interface.ts
@@ -1,7 +1,9 @@
 import { User } from "@firebase/auth";
+import { Badge } from "./badge.enum";
 
 export interface VividiUser extends User {
     id: string;
     firstName: string;
     lastName: string;
+    badges: Badge[];
 }
