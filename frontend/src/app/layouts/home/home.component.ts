d1592131dd74e3cdb64c56f4bdbe591631d6f7ae
xprss <vincenzo.sagristano@yahoo.com>
2025-11-21 22:43:34 +0100

    added tabs in home page

--- frontend/src/app/layouts/home/home.component.ts
+++ frontend/src/app/layouts/home/home.component.ts
@@ -1,24 +1,43 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { FeaturesModule } from '../../features/features.module';
 import { EventsService } from 'src/app/core/events.service';
 import { Subscription } from 'rxjs';
 import { VibeManagementService } from 'src/app/core/vibe.service';
 import { ProgressSpinner } from "primeng/progressspinner";
+import { TabsModule } from 'primeng/tabs';
 
 @Component({
   selector: 'v2d-home',
   standalone: true,
-  imports: [SharedModule, FeaturesModule, ProgressSpinner],
+  imports: [SharedModule, FeaturesModule, ProgressSpinner, TabsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit, OnDestroy {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewInit {
   private refreshEsploraEventSubscription: Subscription | undefined = undefined;
   constructor(
     protected readonly vibeManagementService: VibeManagementService,
     private readonly eventsService: EventsService
   ) {}
+
+  public ngAfterViewInit(): void {
+    const video: HTMLVideoElement | null = document.getElementById(
+      'wipVideo'
+    ) as HTMLVideoElement;
+    if (!video) {
+      return;
+    }
+    video.play().catch((error) => {
+      const resume = () => {
+        video.play();
+        document.removeEventListener('click', resume);
+      };
+      document.addEventListener('click', resume, { once: true });
+      document.addEventListener('scroll', resume, { once: true });
+      document.addEventListener('touchStart', resume, { once: true });
+    });
+  }
 
   public ngOnInit(): void {
     // this serves vibes the very moment the component gets initialized
